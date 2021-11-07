fd47e6849715d327e07c39065dad236de480bb84
Richard Hagen <richard.hagen@gmail.com>
2021-11-07 19:51:29 +0100

    simplest possible grpc loader test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const protoLoader = require('@grpc/proto-loader');
+const grpcLibrary = require('@grpc/grpc-js');
+
+const grpcOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+};
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const packageDefinition = protoLoader.loadSync("test.proto", grpcOptions);
+  const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
+  res.status(200).json(packageObject)
 }

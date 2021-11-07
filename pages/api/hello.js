f9e8eb9150164fc00c69135309fe7cd7f3480c29
Richard Hagen <richard.hagen@gmail.com>
2021-11-07 19:57:43 +0100

    use join for protofile

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const protoLoader = require('@grpc/proto-loader');
 const grpcLibrary = require('@grpc/grpc-js');
+import { resolve, join } from "path";
 
 const grpcOptions = {
   keepCase: true,
@@ -10,12 +11,14 @@ const grpcOptions = {
   oneofs: true
 };
 
+const protoFile = join(process.cwd(), "test.proto");
+
 export const config = {
   unstable_includeFiles: ['test.proto'],
 };
 
 export default function handler(req, res) {
-  const packageDefinition = protoLoader.loadSync("test.proto", grpcOptions);
+  const packageDefinition = protoLoader.loadSync( protoFile, grpcOptions);
   const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
   res.status(200).json(packageObject)
 }

bf5edf4d11c42df9a6c8dc0d5876024127c84e54
Richard Hagen <richard.hagen@gmail.com>
2021-11-07 20:04:26 +0100

    test json

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "fs";
+
 const protoLoader = require('@grpc/proto-loader');
 const grpcLibrary = require('@grpc/grpc-js');
-import { resolve, join } from "path";
 
 const grpcOptions = {
   keepCase: true,
@@ -11,14 +12,15 @@ const grpcOptions = {
   oneofs: true
 };
 
-const protoFile = join(process.cwd(), "test.proto");
+const protoFile = "test.proto";
 
 export const config = {
   unstable_includeFiles: ['test.proto'],
 };
 
 export default function handler(req, res) {
-  const packageDefinition = protoLoader.loadSync( protoFile, grpcOptions);
-  const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
-  res.status(200).json(packageObject)
+  // const packageDefinition = protoLoader.loadSync( protoFile, grpcOptions);
+  // const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
+  const test = fs.readFileSync("test.json")
+  res.status(200).json(test)
 }

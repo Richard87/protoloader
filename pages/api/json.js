b78931c48888a7bb0c99644a264cc9bc50088518
Richard Hagen <richard.hagen@gmail.com>
2021-11-07 20:33:57 +0100

    include test.json

--- pages/api/json.js
+++ pages/api/json.js
@@ -1,5 +1,9 @@
 import fs from "fs"
 
+export const config = {
+  unstable_includeFiles: ['test.json'],
+};
+
 export default function handler(req, res) {
   const content = fs.readFileSync( "test.json");
   res.status(200).json(content)

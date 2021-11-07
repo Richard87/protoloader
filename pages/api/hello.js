5edaa1c0a7847b192f6568a7021769c9791f4f29
Richard Hagen <richard.hagen@gmail.com>
2021-11-07 20:14:56 +0100

    remove test.json

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -21,5 +21,5 @@ export const config = {
 export default function handler(req, res) {
   const packageDefinition = protoLoader.loadSync( protoFile, grpcOptions);
   const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
-  res.status(200).json(test)
+  res.status(200).json(packageObject)
 }
